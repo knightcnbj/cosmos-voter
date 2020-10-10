326502b05c84cbc983807842ba1ac7bf286dafcf
Developer Experience team at Tendermint <hello@tendermint.com>
2020-10-10 15:26:46 -0700

    Initialized with Starport

---
+++ x/cosmosvoter/keeper/params.go
@@ -0,0 +1,22 @@
+package keeper
+/*
+// TODO: Define if your module needs Parameters, if not this can be deleted
+
+import (
+	"time"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/knightcnbj/cosmos-voter/x/cosmosvoter/types"
+)
+
+// GetParams returns the total set of cosmosvoter parameters.
+func (k Keeper) GetParams(ctx sdk.Context) (params types.Params) {
+	k.paramspace.GetParamSet(ctx, &params)
+	return params
+}
+
+// SetParams sets the cosmosvoter parameters to the param space.
+func (k Keeper) SetParams(ctx sdk.Context, params types.Params) {
+	k.paramspace.SetParamSet(ctx, &params)
+}
+*/
